835f64fc17035514c574ce794fe8ad44e4686743
YadicksonVasquez <yadicksonvasquez@gmail.com>
2024-04-09 20:17:02 -0400

    Agregar exception handling to return error 401

--- src/main/java/com/smartjob/config/SmartJobSecurityConfig.java
+++ src/main/java/com/smartjob/config/SmartJobSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -42,6 +44,8 @@ public class SmartJobSecurityConfig {
 		http.csrf((csrf) -> csrf.disable());
 		http.headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
 		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+		http.exceptionHandling(
+				(exp) -> exp.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
 		return http.build();
 	}
 
